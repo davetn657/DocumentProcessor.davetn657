bf4f8253896fe7e03b32c3020d13713036a8fb0f
Dave Ngo <davetn657@gmail.com>
2026-03-22 13:25:41 -0400

    Added Phonebook Project

--- DocumentProcessor.davetn657/Program.cs
+++ DocumentProcessor.davetn657/Program.cs
@@ -1,10 +1,9 @@
-﻿namespace DocumentProcessor.davetn657
+﻿namespace DocumentProcessor.davetn657;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        Console.WriteLine("Hello, World!");
     }
 }
