ec201d9d4af3e7d0489d98e423057f17332ace87
Dave Ngo <davetn657@gmail.com>
2026-03-25 15:52:27 -0400

    Database initialization

--- DocumentProcessor.davetn657/Data/PhonebookContext.cs
+++ DocumentProcessor.davetn657/Data/PhonebookContext.cs
@@ -1,13 +1,24 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using DocumentProcessor.davetn657.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace DocumentProcessor.davetn657.Data;
 
 public class PhonebookContext : DbContext
 {
-    public DbSet<PhonebookContext> Contacts { get; set; }
+    public DbSet<PhonebookProperties> Contacts { get; set; }
+    private string DbPath { get; set; }
 
-    public PhonebookContext(DbContextOptions options) : base(options)
+    public PhonebookContext()
     {
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
 
+        DbPath = builder.GetConnectionString("DefaultConnection");
     }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite(DbPath);
 }
